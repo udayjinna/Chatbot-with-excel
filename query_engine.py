73e5206b19ee7d910c7d86aabe8af1be43a32c58
udayjinna <udayjinna24@gmail.com>
2025-07-20 17:49:22 +0530

    Update query_engine.py

--- query_engine.py
+++ query_engine.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 import os
 
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel("models/gemini-1.5-flash")
+model = genai.GenerativeModel(model_name="models/gemini-1.5-flash")
 
 def ask_question(df, question):
     prompt = f"""
